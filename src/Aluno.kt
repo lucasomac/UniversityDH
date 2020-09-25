8f733c0687a81a9b927855e7eff603a782bc009d
Lucas de Oliveira Macedo <lucasomac@outlook.com>
2020-09-25 20:51:57 -0300

    Feature A - Implementação da classe de aluno com seus requisitos.

---
+++ src/Aluno.kt
@@ -0,0 +1,13 @@
+class Aluno(val nome: String, var sobrenome: String, val codigo: Int) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+        other as Aluno
+        if (codigo != other.codigo) return false
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return codigo
+    }
+}
