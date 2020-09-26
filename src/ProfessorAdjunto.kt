95e47b814a134ed176262cb25c62747718eab1ec
Lucas de Oliveira Macedo <lucasomac@outlook.com>
2020-09-25 21:22:47 -0300

    Feature D - Alteração da classe professor para interface e implementação das classes que implementaram de professor com seus requisitos.

---
+++ src/ProfessorAdjunto.kt
@@ -0,0 +1,15 @@
+class ProfessorAdjunto(override val nome: String, override var sobrenome: String, override val codigo: Int) : Professor {
+    override var tempoCasa = 0
+    var qtdHrMonitoria = 0
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+        other as ProfessorAdjunto
+        if (codigo != other.codigo) return false
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return codigo
+    }
+}
