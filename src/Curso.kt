03067234af38c8442ad095fe3b1cdfbea0ff1e31
Lucas de Oliveira Macedo <lucasomac@outlook.com>
2020-09-29 09:48:24 -0300

    Feature H - Alteração da classe Curso para adição dos metodos de adicionar e excluir aluno

--- src/Curso.kt
+++ src/Curso.kt
@@ -1,5 +1,28 @@
-class Curso(val nome: String, val codigo: Int, var professorTitular: ProfessorTitular, var professorAdjunto: ProfessorAdjunto, qtdMaxAluno: Int) {
-    var alunosMtr = listOf<Aluno>()
+class Curso(val nome: String, val codigo: Int, var professorTitular: ProfessorTitular, var professorAdjunto: ProfessorAdjunto, var qtdMaxAluno: Int) {
+    private var qtdAtualAluno = 0
+    var alunosMtr = mutableListOf<Aluno>()
+    fun adicionarUmAluno(umAluno: Aluno): Boolean {
+        when {
+            this.qtdAtualAluno < this.qtdMaxAluno -> {
+                alunosMtr.add(umAluno)
+                qtdAtualAluno++
+                return true
+            }
+            else -> {
+                return false
+            }
+        }
+    }
+
+    fun excluirAluno(umAluno: Aluno) {
+        try {
+//            alunosMtr.filter { it == umAluno }
+            alunosMtr.remove(umAluno)
+        } catch (e: Exception) {
+            println("Aluno não encontrado --> ${e.message}")
+        }
+    }
+
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
         if (javaClass != other?.javaClass) return false
